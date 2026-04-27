1fab0275e417b2d87d8b27266284e58a69d3ce0d
Abhisek-Mohanty <abhisek.mohanty@techfino.in>
2026-04-27 23:23:03 +0530

    project setup

---
+++ backend/app/core/config.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+from typing import List
+
+from databases import DatabaseURL
+from loguru import logger
+from starlette.config import Config
+from starlette.datastructures import CommaSeparatedStrings, Secret
+
+URL_PREFIX = "/api/v1"
+API_PREFIX = "/api/v2"
+WS_PREFIX  = "/ws/v1"
+WEBHOOK_PREFIX  = "/webhook/v1"
+
+JWT_TOKEN_PREFIX = "Bearer"
+
+config = Config(".env")
+
+USERNAME: str = config("USERNAME", cast=str, default="1.0.0")
+PASSWORD: str = config("PASSWORD", cast=str, default="1.0.0")
+
