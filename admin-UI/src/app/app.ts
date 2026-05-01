4c9f37f873bdd4ee1e3f218ebac445692ece3c1b
Abhisek-Mohanty <abhisekm048@gmail.com>
2026-05-01 15:34:25 +0530

    frontend search bar

--- admin-UI/src/app/app.ts
+++ admin-UI/src/app/app.ts
@@ -1,12 +1,35 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('findex-search');
+protected readonly title = signal('findex-search');
+public searchQuery: string = '';
+public typedText: string = '';
+private fullText: string = "Findex.";
+
+ngOnInit(){
+  this.typeEffect()
+}
+
+
+  openSearch(){
+    console.log(this.searchQuery);
+  }
+
+  async typeEffect() {
+    const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+    
+    for (let i = 0; i <= this.fullText.length; i++) {
+      this.typedText = this.fullText.substring(0, i);
+      console.log(this.typedText);
+      await sleep(100); // Adjust speed here (lower is faster)
+    }
+  }
 }
